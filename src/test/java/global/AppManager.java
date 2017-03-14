10f205bc4502651ae3d49c8a0f09e4df22229009
Matisse Hack <matisse.hack@xamarin.com>
2017-03-14 15:09:25 -0700

    Removed platform version capability

--- src/test/java/global/AppManager.java
+++ src/test/java/global/AppManager.java
@@ -111,7 +111,6 @@ public class AppManager {
                 }
 
                 capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
-                capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.1");
                 capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
                 capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
                 capabilities.setCapability("autoAcceptAlerts",true);
